2781cb04f0a084cfa8edd1b961861c8b6be9a308
KoVLya <ilakovunov@gmail.com>
2023-04-07 19:35:01 +0300

    JS React test website

---
+++ react-spa/src/index.js
@@ -0,0 +1,9 @@
+import { BrowserRouter } from 'react-router-dom';
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
