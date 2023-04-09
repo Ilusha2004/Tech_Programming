30c7f46dbfdd6ae9bd0804757d8b3e713a91c6a3
KoVLya <ilakovunov@gmail.com>
2023-04-09 23:32:55 +0300

    test-build

---
+++ build/precache-manifest.62b365a238b456cb9ea7cba9a73aea73.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc2ca135251b06cad00c09098930c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e8009c6136ae9092b4",
+    "url": "/static/css/main.c302ed6b.chunk.css"
+  },
+  {
+    "revision": "dc98033c7bd1c76759e3",
+    "url": "/static/js/2.05de961c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.05de961c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86e8009c6136ae9092b4",
+    "url": "/static/js/main.2008013f.chunk.js"
+  },
+  {
+    "revision": "5a4b6b2d3a0cd60632df",
+    "url": "/static/js/runtime-main.f294181a.js"
+  },
+  {
+    "revision": "f04ebc13cc96f19866a53ae7a5201009",
+    "url": "/static/media/spa-10.f04ebc13.png"
+  },
+  {
+    "revision": "df1c650afb43563d27c1e1f522d77e29",
+    "url": "/static/media/spa-11.df1c650a.png"
+  },
+  {
+    "revision": "120999b6a0612babaf0875265b1976dd",
+    "url": "/static/media/spa-12.120999b6.png"
+  },
+  {
+    "revision": "014efad5b45e0ef7d931cfdaf121344b",
+    "url": "/static/media/spa-7.014efad5.png"
+  },
+  {
+    "revision": "7c5e2f4c740ec039c90f90c0a4fd8698",
+    "url": "/static/media/spa-8.7c5e2f4c.png"
+  },
+  {
+    "revision": "e87429838a401860a29d9cd2b296f7da",
+    "url": "/static/media/spa-9.e8742983.png"
+  },
+  {
+    "revision": "28aa6b70efad1f976e42cfdc2476cbe6",
+    "url": "/static/media/spa.28aa6b70.png"
+  }
+]);
